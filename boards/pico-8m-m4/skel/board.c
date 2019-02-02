4a708a13ea8fbcf95e0a072399f664e835101be4
Daniel Trnka <daniel.trnka@gmail.com>
2019-02-02 22:27:14 +0100

    BOARD_InitMemory() disables DDRAM except rpmsg region by default

--- boards/pico-8m-m4/skel/board.c
+++ boards/pico-8m-m4/skel/board.c
@@ -67,95 +67,23 @@ void BOARD_InitMemory(void)
     /* Disable the MPU. */
     MPU->CTRL = 0;
 
-    /* 
-       The ARMv7-M default address map define the address space 0x20000000 to 0x3FFFFFFF as SRAM with Normal type, but there the address
-       space 0x28000000 ~ 0x3FFFFFFF has been physically mapped to smart subsystems, so there need change the default memory attributes.
-       Since the base address of MPU region should be multiples of region size, to make it simple, the MPU region 0 set the all 512M of SRAM space
-       with device attributes, then disable subregion 0 and 1 (address space 0x20000000 ~ 0x27FFFFFF) to use the background memory attributes。
-    */
-
-    /* Select Region 0 and set its base address to the M4 code bus start address. */
+    /* configure full access to TCMU + peripherals, etc ~512M - 2^29 */
     MPU->RBAR = (0x20000000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (0 << MPU_RBAR_REGION_Pos);
+    MPU->RASR = (0x3 << MPU_RASR_AP_Pos) | (28 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
 
-    /* Region 0 setting:
-    * 1) Enable Instruction Access;
-    * 2) AP = 011b, full access;
-    * 3) Non-shared device;
-    * 4) Region Not Shared;
-    * 5) Sub-Region 0,1 Disabled;
-    * 6) MPU Protection Region size = 512M byte;
-    * 7) Enable Region 0.
-    */
-    MPU->RASR =
-        (0x1 << MPU_RASR_XN_Pos) | (0x3 << MPU_RASR_AP_Pos) | (0x2 << MPU_RASR_TEX_Pos) | (0x3 << MPU_RASR_SRD_Pos) | (28 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
+    /* configure full aceess to code TCML (256 kbytes - 2^18) */
+    MPU->RBAR = (0x1FFF0000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (1 << MPU_RBAR_REGION_Pos);
+    MPU->RASR = (0x3 << MPU_RASR_AP_Pos) | (17 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
 
-    /* 
-       Non-cacheable area is provided in DDR memory, the DDR region 2MB - 128MB totally 126MB is revserved for CM4 cores. 
-       You can put global or static uninitialized variables in NonCacheable section(initialized variables in NonCacheable.init section)
-       to make them uncacheable. Since the base address of MPU region should be multiples of region size, to make it simple, 
-       the MPU region 1 & 2 set all DDR address space 0x40000000 ~ 0xBFFFFFFF to be non-cacheable). Then MPU region 3 set the text
-       and data section to be cacheable if the program running on DDR. The cacheable area base address should be multiples of its size
-       in linker file, they can be modified per your needs.
-    */
+    /* configure access to rpmsg in DDR (1 MB - 2^20) */
+    MPU->RBAR = (0xB8000000 & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (2 << MPU_RBAR_REGION_Pos);
+    MPU->RASR = (0x3 << MPU_RASR_AP_Pos) | (19 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
 
-    /* Select Region 1 and set its base address to the DDR start address. */
-    MPU->RBAR = (0x40000000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (1 << MPU_RBAR_REGION_Pos);
+    /* enable MemManage handlers */
+    SCB->SHCSR |= SCB_SHCSR_MEMFAULTENA_Msk;
+    /* enable mpu */
+    MPU->CTRL = MPU_CTRL_ENABLE_Msk;
 
-    /* Region 1 setting:
-    * 1) Enable Instruction Access;
-    * 2) AP = 011b, full access;
-    * 3) Shared Device;
-    * 4) MPU Protection Region size = 1024M byte;
-    * 5) Enable Region 1.
-    */
-    MPU->RASR =
-        (0x3 << MPU_RASR_AP_Pos) | (0x1 << MPU_RASR_B_Pos) | (29 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
-
-    /* Select Region 2 and set its base address to the DDR start address. */
-    MPU->RBAR = (0x80000000U & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (2 << MPU_RBAR_REGION_Pos);
-
-    /* Region 2 setting:
-    * 1) Enable Instruction Access;
-    * 2) AP = 011b, full access;
-    * 3) Shared Device;
-    * 4) MPU Protection Region size = 1024M byte;
-    * 5) Enable Region 2.
-    */
-    MPU->RASR =
-        (0x3 << MPU_RASR_AP_Pos) | (0x1 << MPU_RASR_B_Pos) | (29 << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
-
-    while ((size >> i) > 0x1U)
-    {
-        i++;
-    }
-
-    /* If run on DDR, configure text and data section to be cacheable */
-    if(i != 0)
-    {
-        /* The MPU region size should be 2^N, 5<=N<=32, region base should be multiples of size. */
-        assert((size & (size-1)) == 0);
-        assert(!(cacheStart % size));
-        assert(size == (uint32_t)(1 << i));
-        assert(i >= 5);
-
-        /* Select Region 3 and set its base address to the cache able region start address. */
-        MPU->RBAR = (cacheStart & MPU_RBAR_ADDR_Msk) | MPU_RBAR_VALID_Msk | (3 << MPU_RBAR_REGION_Pos);
-
-        /* Region 3 setting:
-        * 1) Enable Instruction Access;
-        * 2) AP = 011b, full access;
-        * 3) Outer and inner Cacheable, write and read allocate;
-        * 4) Region Not Shared;
-        * 5) All Sub-Region Enabled;
-        * 6) MPU Protection Region size get from linker file;
-        * 7) Enable Region 3.
-        */
-        MPU->RASR = 
-             (0x3 << MPU_RASR_AP_Pos) | (0x1 << MPU_RASR_TEX_Pos) | (0x1 << MPU_RASR_C_Pos) | (0x1 << MPU_RASR_B_Pos) | ((i-1) << MPU_RASR_SIZE_Pos) | MPU_RASR_ENABLE_Msk;
-    }
-
-    /* Enable Privileged default memory map and the MPU. */
-    MPU->CTRL = MPU_CTRL_ENABLE_Msk | MPU_CTRL_PRIVDEFENA_Msk;
     /* Memory barriers to ensure subsequence data & instruction
     * transfers using updated MPU settings.
     */
@@ -183,4 +111,25 @@ void BOARD_RdcInit(void)
     CLOCK_ControlGate(kCLOCK_SysPll1Div3Gate,kCLOCK_ClockNeededAll); /* Enable SysPLL1Div3 to Domain 1 */
     CLOCK_ControlGate(kCLOCK_SysPll1Div6Gate,kCLOCK_ClockNeededAll); /* Enable SysPLL1Div6 to Domain 1 */
 
+}
+
+__attribute__((weak)) void HardFault_Handler(void) {
+    printf("hard fault\r\n");
+    for(;;);
+}
+
+
+__attribute__((weak)) void MemManage_Handler(void) {
+    // [0] - fetch from non-executable location
+    // [1] - load/store from non-accessible location
+    uint32_t cfsr    = SCB->CFSR;
+
+    uint32_t mmfar = SCB->MMFAR; // address of location that generated memmanage fault
+    uint32_t bfar    = SCB->BFAR;    // address of location that generated busfault
+
+    printf("MemManage fault\r\n");
+    printf("cfsr = %x\r\n", cfsr);
+    printf("mmfar = %x\r\n", mmfar);
+    printf("bfar = %x\r\n", bfar);
+    for(;;);
 }
